186605aa09385522f42addb89a81576ffd2fc5fd
Luciano Viana <lucianofvianati@gmail.com>
2026-01-05 03:11:00 -0300

    Carregando dataset

--- exam_score_prediction.R
+++ exam_score_prediction.R
@@ -1,7 +1,17 @@
-library(readxl)
+#Análise de dados sobre fatores que contribuem para o desempenho do exame do aluno
 
-arquivo <- file.choose()
+#Dataset https://www.kaggle.com/datasets/kundanbedmutha/exam-score-prediction-dataset
 
-dados <- read.csv(arquivo)
 
-head(dados)
+setwd('D:/Documentos/Dados R')
+getwd()
+
+
+# Carrega o dataset
+dataset = read.table('Exam_Score_Prediction.csv', header = TRUE, sep = ',')
+
+#View(dataset)
+str(dataset)
+
+head(dataset)
+
